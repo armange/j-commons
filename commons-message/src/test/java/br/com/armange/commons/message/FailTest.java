40be2034e562bc2b674a234e0b256c8649e019f5
Diego Armange Costa <diego.armange.costa@gmail.com>
2020-02-09 11:26:39 -0300

    (commons)Feature: Copy gradle to all subprojects

---
+++ commons-message/src/test/java/br/com/armange/commons/message/FailTest.java
@@ -0,0 +1,12 @@
+package br.com.armange.commons.message;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FailTest {
+
+    @Test
+    public void failTest() {
+        Assert.fail("Testing GH Actions");
+    }
+}
---
+++ gradle/shell/export_project_name.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PROJECT_NAME=$1
+
+echo "Current project: $1"
