adaf439acc1891915b7d571d1ff6364cc8f31a93
Diego Armange Costa <diego.armange.costa@gmail.com>
2020-03-03 00:30:46 -0300

    (commons-object-impl)Feature: Converters (skip ci)

---
+++ commons-object-impl/src/main/java/br/com/armange/commons/object/impl/typeconverter/booleanvalue/BooleanToShortConverterImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright [2019] [Diego Armange Costa]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+package br.com.armange.commons.object.impl.typeconverter.booleanvalue;
+
+import br.com.armange.commons.object.api.typeconverter.TypeConverter;
+import br.com.armange.commons.object.api.typeconverter.booleanvalue.BooleanToShortConverter;
+
+public class BooleanToShortConverterImpl implements BooleanToShortConverter {
+
+    private Short result;
+    
+    @Override
+    public TypeConverter<Boolean, Short> from(final Boolean sourceObject) {
+        result = sourceObject != null ? toNumber(sourceObject) : null;
+        
+        return this; 
+    }
+
+    private short toNumber(final Boolean sourceObject) {
+        return sourceObject ? (short)1 : (short)0;
+    }
+
+    @Override
+    public Short to(final Class<Short> targetClass) {
+        return result;
+    }
+
+    @Override
+    public boolean matches(final Object sourceObject, final Class<?> targetClass) {
+        return sourceObject.getClass().equals(Boolean.class) && targetClass.equals(Short.class);
+    }
+}
