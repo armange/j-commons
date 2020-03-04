cb6ebb37061deb1bb43a599c12b42e987d05fcff
Diego Armange Costa <diego.armange.costa@gmail.com>
2020-03-04 00:36:55 -0300

    (commons-object-impl)Feature: Converters (skip ci)

---
+++ commons-object-impl/src/main/java/br/com/armange/commons/object/impl/typeconverter/bytevalue/ByteToCharactereConverterImpl.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright [2019] [Diego Armange Costa]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+package br.com.armange.commons.object.impl.typeconverter.bytevalue;
+
+import br.com.armange.commons.object.api.typeconverter.TypeConverter;
+import br.com.armange.commons.object.api.typeconverter.bytevalue.ByteToCharactereConverter;
+
+public class ByteToCharactereConverterImpl implements ByteToCharactereConverter {
+
+    private Character result;
+    
+    @Override
+    public TypeConverter<Byte, Character> from(final Byte sourceObject) {
+        result = sourceObject != null ? ((char)sourceObject.intValue()) : null;
+        
+        return this; 
+    }
+
+    @Override
+    public Character to(final Class<Character> targetClass) {
+        return result;
+    }
+
+    @Override
+    public boolean matches(final Object sourceObject, final Class<?> targetClass) {
+        return sourceObject.getClass().equals(Byte.class) && targetClass.equals(Character.class);
+    }
+}
---
+++ commons-object-impl/src/main/java/br/com/armange/commons/object/impl/typeconverter/bytevalue/ByteToIntegerConverterImpl.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright [2019] [Diego Armange Costa]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+package br.com.armange.commons.object.impl.typeconverter.bytevalue;
+
+import br.com.armange.commons.object.api.typeconverter.TypeConverter;
+import br.com.armange.commons.object.api.typeconverter.bytevalue.ByteToIntegerConverter;
+
+public class ByteToIntegerConverterImpl implements ByteToIntegerConverter {
+
+    private Integer result;
+    
+    @Override
+    public TypeConverter<Byte, Integer> from(final Byte sourceObject) {
+        result = sourceObject != null ? sourceObject.intValue() : null;
+        
+        return this; 
+    }
+
+    @Override
+    public Integer to(final Class<Integer> targetClass) {
+        return result;
+    }
+
+    @Override
+    public boolean matches(final Object sourceObject, final Class<?> targetClass) {
+        return sourceObject.getClass().equals(Byte.class) && targetClass.equals(Integer.class);
+    }
+}
